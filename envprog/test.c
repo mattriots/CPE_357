95d416ee402eba8d345f20dfe87d564bc16033e7
mattriots <mattdepauw@gmail.com>
2022-03-28 15:40:25 -0700

    added test files

---
+++ envprog/test.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+void main()
+{
+    int a;
+    a = 9;
+    int arr[10];
+    int i;
+    for(i=0;i<10;i++)
+        arr[i] = i*2;
+
+    printf("%d\n",arr[1]);
+    printf("I am alive");
+}
