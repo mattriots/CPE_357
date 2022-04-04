205dd0501458228c012da362bfe0698f8b26fbe4
mattriots <mattdepauw@gmail.com>
2022-04-04 12:45:48 -0700

    read/write files in C/C++

---
+++ virtual_memory/part_a/padding_files/padding.cpp
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <strings.h>
+#include <iostream>
+#include <fstream> //for reading files in C++
+
+using namespace std;
+
+
+typedef unsigned char BYTE;
+typedef struct mystruct
+{
+    BYTE b;
+    int i;
+} mystruct;
+
+int main()
+{
+    //C++ Notation
+    ifstream file("test.bin"); //opens file
+    // file.open("test.bin"); //another way to do it
+    
+    if(!file.is_open()){  //check to see if file exists or is open?
+        return 0;
+    }
+    mystruct s;
+
+    //read into file
+    file >> s.b;
+    file >> s.i;
+
+    file.close();
+
+    //write to file
+    ofstream ofile("out.bin");
+    ofile << s.b;
+    ofile << s.i;
+
+    ofile.close();
+
+
+
+
+
+    //C Notation
+    // read bytefile in
+    FILE *file = fopen("test.bin", "rb"); //rb = read byte
+    mystruct s; //instace of struct
+        //fread(reading into, size, how many times, from where)
+    fread(&s.b, sizeof(s.b), 1, file);
+    fread(&s.i, sizeof(s.i), 1, file); //have to read them in individually
+                                        //or padding will throw us off
+
+    fclose(file);
+    cout << (int)s.b << endl;
+    cout << s.i << endl;
+
+    // Write bytefile out
+    /* FILE *file = fopen("test.bin", "wb"); //open a file
+                    // fopen("file", "what to do with file")
+                    // w = write, r = read, wb = write binary, rb = read binary
+
+     BYTE y = 5;
+     int i = 99;
+        //fwrite(address to write to, how much, how many times, from file pointer)
+     fwrite(&y, 1, 1, file);
+     fwrite(&i, 4, 1, file);
+
+
+     fclose(file);    */
+    // close file
+
+    return 0;
+}
